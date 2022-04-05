befb87aa18ef09e2894079502e64ac2dee1497d7
Vladyslav Shtompel <vladyslav.shtompel@gmail.com>
2022-04-05 17:13:49 +0300

    Добавлены выполнения всех задания в ДЗ

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,30 @@
+import throttle from 'lodash.throttle'
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+
+const savedTime = localStorage.getItem("videoplayer-current-time");
+const parsedTime = JSON.parse(savedTime);
+
+player.on('timeupdate', throttle(handleTimeUpdate, 1000));
+
+function handleTimeUpdate(currentTime) {
+
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(currentTime.seconds));
+}
+
+player.setCurrentTime(parsedTime).then(function(seconds) {
+    
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+           
+            break;
+
+        default:
+            
+            break;
+    }
+});
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,67 @@
+import throttle from 'lodash.throttle'
+
+const form = document.querySelector('.feedback-form');
+const input = document.querySelector('input');
+const textArea = document.querySelector('textarea');
+const submit = document.querySelector('button');
+
+form.addEventListener("input", throttle(handleInput, 500));
+
+submit.addEventListener('click', handleSubmit);
+
+const savedForm = localStorage.getItem("feedback-form-state");
+const parsedForm = JSON.parse(savedForm);
+
+let objForShow;
+
+function isEmptyEmail() {
+    if(parsedForm.email === undefined){
+        return input.value = "";   
+    }
+    input.value = parsedForm.email;
+}
+
+function isEmptyMessage() {
+    if(parsedForm.message === undefined) {
+        return textArea.value = "";
+    }
+    textArea.value = parsedForm.message;
+}
+
+function getForm() {
+    if(savedForm) {
+    isEmptyEmail();
+    isEmptyMessage();
+    }
+}
+
+function handleInput(e) {
+    let email = form.elements.email.value;
+    let message = form.elements.message.value;
+
+    const formData = {
+        email,
+        message
+    }
+
+    objForShow = formData;
+
+    localStorage.setItem("feedback-form-state", JSON.stringify(formData));
+}
+
+function handleSubmit(e) {
+    e.preventDefault();
+
+    console.log(objForShow);
+    localStorage.removeItem("feedback-form-state");
+    form.reset();
+    
+}
+
+getForm();
+
+
+
+
+
 
